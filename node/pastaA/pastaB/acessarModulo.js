8d8878d9b54ac66dad34625ff06c7cc562f98a87
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-10 17:55:02 -0300

    Sistema de Módulos: Require
    É importado o módulo "moduloA.js" usando um caminho relativo, subindo duas pastas para acessá-lo.
    
    Em seguida, é exibido a propriedade "ola" do módulo importado no console.
    
    Depois, estou importando um módulo ou diretório local chamado "pastaC". Este import pode estar vinculado a um arquivo "index.js" dentro do diretório.
    
    Há um trecho de código comentado que cria um servidor HTTP, mas não está atualmente em execução.
    
    Por fim, é exibido no console o que foi importado do diretório "pastaC".

---
+++ node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,21 @@
+// Importa o módulo "moduloA.js" de um caminho relativo (subindo duas pastas e acessando o arquivo)
+const moduloA = require("../../moduloA.js");
+
+// Exibe no console a propriedade "ola" do módulo importado de "moduloA.js"
+console.log(moduloA.ola);
+
+// Importa um módulo ou diretório local chamado "pastaC" (provavelmente um arquivo "index.js" dentro desse diretório)
+const c = require("./pastaC/");
+
+// O código a seguir está atualmente comentado, mas se descomentado, criaria um servidor HTTP
+// usando o módulo "http" do Node.js para responder a solicitações com "Boa tarde!" na porta 8080.
+// O servidor não está em execução no momento.
+
+// const http = require("http");
+// http.createServer((req, res) => {
+//    res.write("Boa tarde!");
+//    res.end();
+// }).listen(8080);
+
+// Exibe no console o que foi importado do diretório "pastaC"
+console.log(c);
