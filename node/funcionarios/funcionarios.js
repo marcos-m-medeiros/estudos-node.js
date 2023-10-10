13c71db9319677f4a9d9553052004f3e9ee864de
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-10 19:53:07 -0300

    Desafio package.json
    É importada a biblioteca Axios para fazer requisições HTTP.
    
    Em seguida, define três funções:
    
    - `chineses` filtra funcionários da China.
    - `mulheres` filtra funcionários do gênero feminino.
    - `menorSalario` é uma função de redução que compara dois funcionários e retorna o funcionário com o menor salário.
    
    A requisição HTTP é feita para obter os dados dos funcionários da URL especificada.
    
    Após obter os dados, é encadeado os métodos filter para filtrar os funcionários chineses e do gênero feminino.
    
    Finalmente, é usado o método reduce com a função menorSalario para encontrar o funcionário com o menor salário entre os funcionários filtrados e o exibe no console.

---
+++ node/funcionarios/funcionarios.js
@@ -0,0 +1,24 @@
+// Importa a biblioteca Axios para fazer requisições HTTP.
+const axios = require("axios");
+
+// Define funções de filtro e redução para encontrar o funcionário com o menor salário.
+const chineses = (f) => f.pais === "China";
+const mulheres = (f) => f.genero === "F";
+const menorSalario = (func, funcAtual) => {
+  return func.salario < funcAtual.salario ? func : funcAtual;
+};
+
+// Faz uma requisição HTTP para obter os dados dos funcionários.
+axios.get(url).then((response) => {
+  // Obtém os dados dos funcionários da resposta.
+  const funcionarios = response.data;
+
+  // Filtra os funcionários para encontrar os chineses e depois as mulheres.
+  const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario);
+
+  // Exibe o funcionário com o menor salário no console.
+  console.log(func);
+});
