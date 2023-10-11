62d762d4f7e706b33a03ee2997e6e81a1e8e99c5
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-10-10 23:08:22 -0300

    Passando Parâmetros entre Módulos
    Este código demonstra como importar um módulo e chamar uma função exportada desse módulo, passando argumentos para essa função. No caso, a função exportada cria saudações personalizadas com os nomes fornecidos e retorna um array de saudações, que é então impresso no console.

---
+++ node/passandoParamentrosCliente.js
@@ -0,0 +1,5 @@
+// Requer o módulo "passandoParametros" e chama a função exportada, passando os nomes "Ana", "Lucas" e "João" como argumentos.
+const saudacoes = require("./passandoParametros")("Ana", "Lucas", "João");
+
+// Imprime a variável "saudacoes" no console.
+console.log(saudacoes);
